cab74a50ef959abdc21adf4fb90cbc28cc688268
jeanfelix01 <jeanfelix_@hotmail.com>
2022-01-25 12:02:28 -0300

    Simplificano Script

--- autobackup.sh
+++ autobackup.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
- 
+
+
 ORIGEM=$1
-DESTINO=$2  
+DESTINO=$2
+DATA=(date +%d-%m-%Y" "%H:%M:%S)
 
-if [[ ! -d "$DESTINO" ]]; then  
-    mkdir "$DESTINO"; 
-fi
+#if [[ ! -d "$DESTINO" ]]; then  
+#    mkdir "$DESTINO"; 
+#fi
+
+#echo $ORIGEM
+#echo $DESTINO
 
 
-(
 echo Inicio do Backup de $(date +%d-%m-%Y" "%H:%M:%S)
 echo     
-tar -cvzf "$(basename $(dirname $ORIGEM)).$(date +%Y-%b-%d.%H%M).tar.gz" $ORIGEM
-mv "$(basename $(dirname $ORIGEM)).$(date +%Y-%b-%d.%H%M).tar.gz" $DESTINO
+tar -cvzf "$DESTINO/$(basename $(dirname $ORIGEM)).$DATA.tar.gz" "$ORIGEM"
 echo
 echo "---Fim do Backup---"
 echo
-)
-
 
 
 
